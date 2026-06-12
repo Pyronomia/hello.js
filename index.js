ce7fe2b04bebbf9de3911b7fa15f1ffac62250ec
Akintayo Sinmiloluwa <akintayosinmi1@gmail.com>
2026-06-12 22:40:16 +0100

    Assignment Done

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const name = "Sinmi";
 
-console.log(name);
+console.log("hello",name);
+// If you want more info on all the remaining things discussed just go back to your js tutorial
+const user = {
+    name: "Sinmi",
+    age: 18
+}
+console.log(user["name"])
+
+function multiplyAll(arr1, arr2){
+    let result;
+    for(i = 0; i < arr1.length; i++){
+        for(j = 0; j < arr2.length; j++){
+            result = arr1[i] * arr2[j];
+        }
+    }
+
+    return result;
+}
+
+console.log(multiplyAll([1,2,3], [4,5,6]))
---
+++ intro.js
@@ -0,0 +1,7 @@
+const info = {
+    name: "Sinmi",
+    age: 18,
+    "favourite programming language": "JavaScript"
+}
+
+console.log(info)
